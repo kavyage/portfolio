23cb9cbcf218324577324f21364c13eb73509ff2
Kavya <116964714+kavyage@users.noreply.github.com>
2025-12-05 22:58:14 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ function Navbar() {
             <img src={pic} className="h-12 w-12 rounded-full" alt="" />
             <h1 className="font-semibold text-xl cursor-pointer">
               KAVYA<span className="text-green-500 text-2xl"> ?</span>
-              <p className="text-sm">Web Developer</p>
+              <p className="text-sm">SDET</p>
             </h1>
           </div>
           {/* desktop navbar */}
