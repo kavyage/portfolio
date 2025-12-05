b2b3ac29cec50b6a7e2e9ace9d5ce6ff3dc620b2
Kavya <116964714+kavyage@users.noreply.github.com>
2025-12-05 23:06:18 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,12 +12,7 @@ function Footer() {
       <footer className="py-12">
         <div className="max-w-screen-2xl container mx-auto px-4 md:px-20">
           <div className=" flex flex-col items-center justify-center">
-            <div className="flex space-x-4">
-              <FaFacebook size={24} />
-              <FaTwitter size={24} />
-              <FaInstagram size={24} />
-              <FaLinkedinIn size={24} />
-            </div>
+      
             <div className="mt-8 border-t border-gray-700 pt-8 flex flex-col items-center">
               <p className="text-sm">
                 &copy; 2024 Your Company. All rights reserved.
