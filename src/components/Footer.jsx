ca70e9ec7be179d8feddb6c21fa002ad4b1661ff
Kavya <116964714+kavyage@users.noreply.github.com>
2025-12-06 17:46:22 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
         <div className="max-w-screen-2xl container mx-auto px-4 md:px-20">
           <div className=" flex flex-col items-center justify-center">
       
-            <div className="mt-8 border-t border-gray-700 pt-8 flex flex-col items-center">
+         
               <p className="text-sm">
                 &copy; 2024 Kavya's Portfolio. All rights reserved.
               </p>
