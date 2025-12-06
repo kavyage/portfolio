95873e7b6a6c7f42f40d3d9621506a95b3cf7c80
Kavya <116964714+kavyage@users.noreply.github.com>
2025-12-06 17:53:16 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,12 @@ import React from "react";
 function Footer() {
   return (
     <>
-      <hr />
+ 
       <footer className="py-12">
         <div className="max-w-screen-2xl container mx-auto px-4 md:px-20">
           <div className=" flex flex-col items-center justify-center">
       
-            <div className="mt-8 pt-8 flex flex-col items-center">
+<div className="mt-8 border-t border-gray-700 pt-8 flex flex-col items-center">
               <p className="text-sm">
                 &copy; 2025 Kavya's Portfolio. All rights reserved.
               </p>
